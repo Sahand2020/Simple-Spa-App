5fc021962f7ef55735c1548d51b10c380fe7988a
Sahand <76976809+Sahand2020@users.noreply.github.com>
2022-03-24 21:40:28 +0430

    all project pushed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from './App';
+import reportWebVitals from "./reportWebVitals";
+// import App1 from './App-Class';
+// import App2 from './App-Func';
+import Landing from './Landing';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <App1/> */}
+    {/* <App2/> */}
+    <Landing />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
