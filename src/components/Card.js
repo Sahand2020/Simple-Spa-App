111cff235f2e754559a537f3d22e2771b80b4562
Sahand <76976809+Sahand2020@users.noreply.github.com>
2022-03-29 00:14:58 +0430

    card component updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,49 @@
 import React, { Component } from "react";
 import styles from "./Card.module.css";
+import down from "../images/down.svg";
+import up from "../images/up.svg";
 
 class Card extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  downHandler = () => {
+    if (this.state.counter >= 1) {
+      this.setState((prevState) => ({
+        counter: prevState.counter - 1,
+      }));
+    }
+  };
+
+  upHandler = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+
   render() {
     const { image, name, cost } = this.props;
+    const { counter } = this.state;
     return (
       <div className={styles.container}>
         <img src={image} alt="smart phone" />
         <h3>{name}</h3>
         <p>{cost}</p>
+
+        <div className={styles.counter}>
+          <img
+            src={down}
+            alt="arrow"
+            className={counter ? "" : styles.deactivated}
+            onClick={this.downHandler}
+          />
+          <span>{counter}</span>
+          <img src={up} alt="arrow" onClick={this.upHandler} />
+        </div>
       </div>
     );
   }
