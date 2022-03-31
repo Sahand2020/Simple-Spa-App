004c697a023e22dfc3b2eaadabfdbb04c933f3ab
Sahand <76976809+Sahand2020@users.noreply.github.com>
2022-03-31 17:20:47 +0430

    card counter updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -32,13 +32,19 @@ class Card extends Component {
       <div className={styles.container}>
         <img src={image} alt="smart phone" />
         <h3>{name}</h3>
-        <p>{cost}</p>
+        <p>
+          {cost}{" "}
+          {counter
+            ? ` x ${counter} = ${counter * Number(cost.split(" ")[0])} $`
+            : ""}
+        </p>
 
         <div className={styles.counter}>
           <img
             src={down}
             alt="arrow"
-            className={counter ? "" : styles.deactivated}
+            // className={counter ? "" : styles.deactivated}
+            className={!counter && styles.deactivated}
             onClick={this.downHandler}
           />
           <span>{counter}</span>
